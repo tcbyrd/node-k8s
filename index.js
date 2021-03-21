c957fcb0779b106ecb541878334816cfbf04525a
Tommy Byrd <thomascbyrd@gmail.com>
2021-03-20 21:10:26 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const http = require('http')
 
 http.createServer((req, res) => {
+  if (req.path === '/.github/version') {
+    return res.end(process.env.DEPLOYED_SHA);
+  }
   res.end('foo')
 }).listen(3000)
