736d08dfa6debb6fa11c7373ecd323f24b3501bb
Tommy Byrd <thomascbyrd@gmail.com>
2021-03-21 00:39:43 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ http.createServer((req, res) => {
   if (req.url === '/.github/version') {
     return res.end(process.env.GITHUB_SHA);
   }
-  res.end('Testing the GitHub flow!')
+  res.end('Testing deployment to staging!')
 }).listen(3000)
